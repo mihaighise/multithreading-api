95b99825f25c79de156df9dffc7daf4b59a0e73d
mihaighise <mihaighise1@gmail.com>
2023-08-06 21:43:02 +0300

    first commit

---
+++ src/test/java/com/mihaighise/multithreadingapi/MultithreadingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihaighise.multithreadingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
